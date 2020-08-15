fd0aa84f5c9faeb6008920268270e8946ba7ad08
kalikane <kanewadel32@gmail.com>
2020-08-15 17:41:33 +0100

    Blog create with django

---
+++ .envblog/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\kola\documents\phyton\django_project\blog\.envblog\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
